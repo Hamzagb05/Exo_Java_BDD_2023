56f3d082edb0f05e6859545ab476705ca4c29f1b
Hamzagb05 <chatir85@gmail.com>
2025-04-28 11:33:44 +0100

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -7,34 +7,29 @@ public class Task implements Serializable {
     private String title;
     private String description;
     private LocalDate dueDate;
-    private boolean isCompleted;
+    private boolean completed;
 
     public Task(String title, String description, LocalDate dueDate) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
-        this.isCompleted = false;
+        this.completed = false;
     }
 
-    // Getters
+    // Getter et Setter
     public String getTitle() {
         return title;
     }
-
     public String getDescription() {
         return description;
     }
-
     public LocalDate getDueDate() {
         return dueDate;
     }
-
     public boolean isCompleted() {
-        return isCompleted;
+        return completed;
     }
-
-    // Setters
     public void setCompleted(boolean completed) {
-        isCompleted = completed;
+        this.completed = completed;
     }
 }
